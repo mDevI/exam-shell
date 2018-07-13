7f39eb9e0a83339d0b9a3ee30a926f456e8aa9fc
Sergei.Belonosov <2belonosov@gmail.com>
2018-07-13 19:53:27 +0700

    Initial commit

---
+++ src/main/java/com/mdevi/examshell/ExamShellApplication.java
@@ -0,0 +1,12 @@
+package com.mdevi.examshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamShellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamShellApplication.class, args);
+    }
+}
